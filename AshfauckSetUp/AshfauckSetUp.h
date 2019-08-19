90e94319b24849b27eaa97cb9045246bdf21f740
ashfauck t <ashfauck.orgware@gmail.com>
2019-08-19 13:14:01 +0530

    Initial Commit

---
+++ AshfauckSetUp/AshfauckSetUp.h
@@ -0,0 +1,19 @@
+//
+//  AshfauckSetUp.h
+//  AshfauckSetUp
+//
+//  Created by Ashfauck on 8/19/19.
+//  Copyright © 2019 orgware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AshfauckSetUp.
+FOUNDATION_EXPORT double AshfauckSetUpVersionNumber;
+
+//! Project version string for AshfauckSetUp.
+FOUNDATION_EXPORT const unsigned char AshfauckSetUpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AshfauckSetUp/PublicHeader.h>
+
+
